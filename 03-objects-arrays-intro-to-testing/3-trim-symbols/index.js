2b81473b0c0129e2efedc9981b04878d5768a00d
vaxet <idletoiler@gmail.com>
2024-11-30 23:29:56 +0300

    done

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    if (size === null || size === undefined){
+        return string;
+    }
+    let ans = "";
+    let prevSymb = null;
+    let count = 0;
+    for (let i = 0; i < string.length; ++i){
+        if (prevSymb === string[i]){
+            if (count >= size){
+                continue;
+            }
+            count++;
+            ans = ans + prevSymb;
+        } else {
+            count = 1;
+            prevSymb = string[i];
+            if (size > 0){
+                ans = ans + prevSymb;
+            }
+        }
+    }
+    return ans;
 }
